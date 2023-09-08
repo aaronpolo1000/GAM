7ba8e83e03ddcc59411333f3cc566fa6f6937e28
aaronpolo1000 <aaronpolo1000@gmail.com>
2023-09-08 15:07:16 -0300

    a

--- Project1/Project1/Game1.cs
+++ Project1/Project1/Game1.cs
@@ -14,23 +14,23 @@ namespace Project1
         // Encarga de dibujar las texturas en pantalla.
         private SpriteBatch _spriteBatch;
         // Crea la variable de textura 2D "pruebas".
-        private Texture2D _pruebasTexture;
+        private Texture2D _p1Texture;
         // Crea la variable de posición de "pruebas"
-        private Vector2 _pruebasPosition;
+        private Vector2 _p1Position;
         // Crea la variable de la velocidad en la que se moverá el sprite.
-        private Vector2 _pruebasSpeed = new Vector2(3, 3);
-
-        private Texture2D _manzanaTexture;
-        private Vector2 _manzanaPosition;
+        private Vector2 _p1Speed = new Vector2(3, 3);
+        private Vector2 _p2Speed = new Vector2(3, 3);
+        private Texture2D _p2Texture;
+        private Vector2 _p2Position;
         private Texture2D _backgroundTexture;
         private Texture2D _bochaTexture;
         private Vector2 _bochaPosition;
+        private Vector2 _bochaSpeed= new Vector2(0,0);
         private bool _bochatocada=false, a,b,c,d;
 
 
 
 
-        private bool _manzanaTocada = false; // Bandera para controlar si se tocó la manzana
 
         public Game1()
         {
@@ -49,98 +49,193 @@ namespace Project1
             // Permite calcular los graficos(?)
             _spriteBatch = new SpriteBatch(GraphicsDevice);
             // Carga la textura "pruebas" en la variable.
-            _pruebasTexture = Content.Load<Texture2D>("a");
+            _p1Texture = Content.Load<Texture2D>("a");
             // Define la posición de inicio del sprite "pruebas".
-            _pruebasPosition = new Vector2(100, 100);
+            _p1Position = new Vector2(100, 100);
 
 
             // Carga las textura "manzana" en la variable.
-            _manzanaTexture = Content.Load<Texture2D>("c");
+            _p2Texture = Content.Load<Texture2D>("c");
             // (?)
-            _manzanaPosition = new Vector2(400, 200);
+            _p2Position = new Vector2(500, 200);
             _bochaTexture = Content.Load<Texture2D>("bocha5");
             _bochaPosition = new Vector2(379, 219);
-
+            _bochaSpeed = new Vector2(0, 0);
 
 
         }
 
         protected override void Update(GameTime gameTime)
         {
+            Rectangle pruebasRectangle = new Rectangle((int)_p1Position.X, (int)_p1Position.Y, 30, 30);
+            // Es la caja de coliciones de "pruebas" y se basa en el tamaño de la imgane.
+            Rectangle manzanaRectangle = new Rectangle((int)_p2Position.X, (int)_p2Position.Y, 30, 30);
+            Rectangle bochaRectangle = new Rectangle((int)_bochaPosition.X, (int)_bochaPosition.Y, 40, 40);
             // Se utiliza para poder recibir el estado del teclado.
             KeyboardState keyboardState = Keyboard.GetState();
 
-            // Se ejecuta cuando la variable _manzanaTocada es "false".
            
-                // Movimiento del sprite "pruebas" controlado por las flechas del teclado
 
-                // Cuando se aprieta la tecla "left" se hace la siguiente operación:
-                if (keyboardState.IsKeyDown(Keys.Left) && _pruebasPosition.X > 48)
+                if (keyboardState.IsKeyDown(Keys.Left) && _p1Position.X > 48)
                 {
-                    // Se resta la posición actual en "X" por el valor de la velocidad. Si velocidad es 2 se restan 2 posiciones.
-                    _pruebasPosition.X -= 3;
+                _p1Speed.X -= 2;
                 }
 
-                // Cuando se aprieta la tecla "right" se hace la siguiente operación:
-                if (keyboardState.IsKeyDown(Keys.Right) && _pruebasPosition.X < 700)
+               else if (keyboardState.IsKeyDown(Keys.Right) && _p1Position.X < 700)
                 {
-                    // Se suma la posición actual en "X" por el valor de la velocidad.
-                    _pruebasPosition.X += 3;
-                }
-
-                // Cuando se aprieta la tecla "up" se hace la siguiente operación:
-                if (keyboardState.IsKeyDown(Keys.Up) && _pruebasPosition.Y > 33)
-                {
-                    // Se resta la posición actual en "Y" por el valor de la velocidad.
-                    _pruebasPosition.Y -= 3;
-                }
-
-                // Cuando se aprieta la tecla "down" se hace la siguiente operación:
-                if (keyboardState.IsKeyDown(Keys.Down) && _pruebasPosition.Y < 390)
-                {
-                    // Se suma la posición actual en "Y" por el valor de la velocidad.
-                    _pruebasPosition.Y += 3;
-                }
-           if (keyboardState.IsKeyDown(Keys.A) && _manzanaPosition.X >48&&_bochatocada==false)
-            {
-                
-
-                // Se resta la posición actual en "X" por el valor de la velocidad. Si velocidad es 2 se restan 2 posiciones.
-                _manzanaPosition.X -= 3;
-            }
-
-            // Cuando se aprieta la tecla "right" se hace la siguiente operación:
-            if (keyboardState.IsKeyDown(Keys.D) && _manzanaPosition.X <700)
-            {
                
-                // Se suma la posición actual en "X" por el valor de la velocidad.
-                _manzanaPosition.X += 3;
-            }
 
-            // Cuando se aprieta la tecla "up" se hace la siguiente operacwwwwwión:
-            if (keyboardState.IsKeyDown(Keys.W) && _manzanaPosition.Y > 33)
+                _p1Speed.X += 2;
+                }
+            else
             {
-               
-                // Se resta la posición actual en "Y" por el valor de la velocidad.
-                _manzanaPosition.Y -= 3;
+                _p1Speed.X = 0;
             }
 
-            // Cuando se aprieta la tecla "down" se hace la siguiente operación:
-            if (keyboardState.IsKeyDown(Keys.S) && _manzanaPosition.Y < 390)
+            float friccion1 = 0.9f;
+
+            float friccion = 0.4f;
+            if ( _bochaPosition.X > 48 && _bochaPosition.X < 700)
             {
-                
-                // Se suma la posición actual en "Y" por el valor de la velocidad.
-                _manzanaPosition.Y += 3;
+
+                _bochaPosition.X += _bochaSpeed.X;
+                _bochaSpeed *= friccion1;
+            }
+            _p1Position.X += _p1Speed.X;
+            _p1Speed *= friccion;
+            if (bochaRectangle.Intersects(pruebasRectangle))
+            {
+                _bochaSpeed = Vector2.Normalize(_p1Speed)*5;
+            }
+            _bochaPosition.X += _bochaSpeed.X;
+            _bochaSpeed *= friccion;
+
+
+
+
+
+
+
+
+
+            if (keyboardState.IsKeyDown(Keys.Up) && _p1Position.Y > 33)
+            {
+                _p1Speed.Y -= 2;
             }
 
-            // Verificar colisión con la manzana
-            Rectangle pruebasRectangle = new Rectangle((int)_pruebasPosition.X, (int)_pruebasPosition.Y, 50, 50);
-            Rectangle pruebas1Rectangle = new Rectangle((int)_pruebasPosition.X, (int)_pruebasPosition.Y, 50, 50);
-            // Es la caja de coliciones de "pruebas" y se basa en el tamaño de la imgane.
-            Rectangle manzanaRectangle = new Rectangle((int)_manzanaPosition.X, (int)_manzanaPosition.Y, 50, 50);
-            Rectangle bochaRectangle = new Rectangle((int)_bochaPosition.X, (int)_bochaPosition.Y, 40, 40);
+            else if (keyboardState.IsKeyDown(Keys.Down) && _p1Position.Y < 390)
+            {
 
-            // Es la caja de coliciones de "manzana" y se basa en el tamaño de la imgane.
+
+                _p1Speed.Y += 2;
+            }
+            else
+            {
+                _p1Speed.Y = 0;
+            }
+
+         
+            if (_bochaPosition.Y > 33 && _bochaPosition.Y < 390)
+            {
+
+                _bochaPosition.Y += _bochaSpeed.Y;
+                _bochaSpeed *= friccion1;
+            }
+            _p1Position.Y += _p1Speed.Y;
+            _p1Speed *= friccion;
+            if (bochaRectangle.Intersects(pruebasRectangle))
+            {
+                _bochaSpeed = Vector2.Normalize(_p1Speed) * 5;
+            }
+            _bochaPosition.Y += _bochaSpeed.Y;
+            _bochaSpeed *= friccion;
+
+
+
+
+
+
+
+
+
+
+
+
+            if (keyboardState.IsKeyDown(Keys.A) && _p2Position.X > 48)
+            {
+                _p2Speed.X -= 2;
+            }
+
+            else if (keyboardState.IsKeyDown(Keys.D) && _p2Position.X < 700)
+            {
+
+
+                _p2Speed.X += 2;
+            }
+            else
+            {
+                _p2Speed.X = 0;
+            }
+
+
+            if (_bochaPosition.X > 48 && _bochaPosition.X < 700)
+            {
+
+                _bochaPosition.X += _bochaSpeed.X;
+                _bochaSpeed *= friccion1;
+            }
+            _p2Position.X += _p2Speed.X;
+            _p2Speed *= friccion;
+            if (bochaRectangle.Intersects(manzanaRectangle))
+            {
+                _bochaSpeed = Vector2.Normalize(_p2Speed) * 5;
+            }
+            _bochaPosition.X += _bochaSpeed.X;
+            _bochaSpeed *= friccion;
+
+
+
+
+
+
+
+
+
+            if (keyboardState.IsKeyDown(Keys.W) && _p2Position.Y > 33)
+            {
+                _p2Speed.Y -= 2;
+            }
+
+            else if (keyboardState.IsKeyDown(Keys.S) && _p2Position.Y < 390)
+            {
+
+
+                _p2Speed.Y += 2;
+            }
+            else
+            {
+                _p2Speed.Y = 0;
+            }
+
+
+            if (_bochaPosition.Y > 33 && _bochaPosition.Y < 390)
+            {
+
+                _bochaPosition.Y += _bochaSpeed.Y;
+                _bochaSpeed *= friccion1;
+            }
+            _p2Position.Y += _p2Speed.Y;
+            _p2Speed *= friccion;
+            if (bochaRectangle.Intersects(manzanaRectangle))
+            {
+                _bochaSpeed = Vector2.Normalize(_p2Speed) * 5;
+            }
+            _bochaPosition.Y += _bochaSpeed.Y;
+            _bochaSpeed *= friccion;
+
+
+
+
 
 
 
@@ -155,9 +250,9 @@ namespace Project1
             _spriteBatch.Begin();
             
             _spriteBatch.Draw(_backgroundTexture, new Rectangle(0, 0, GraphicsDevice.Viewport.Width, GraphicsDevice.Viewport.Height), Color.White);
-            _spriteBatch.Draw(_pruebasTexture, new Rectangle((int)_pruebasPosition.X, (int)_pruebasPosition.Y, 50, 50), Color.White);
+            _spriteBatch.Draw(_p1Texture, new Rectangle((int)_p1Position.X, (int)_p1Position.Y, 50, 50), Color.White);
            
-                _spriteBatch.Draw(_manzanaTexture, new Rectangle((int)_manzanaPosition.X, (int)_manzanaPosition.Y, 50,50), Color.White);
+                _spriteBatch.Draw(_p2Texture, new Rectangle((int)_p2Position.X, (int)_p2Position.Y, 50,50), Color.White);
             _spriteBatch.Draw(_bochaTexture,new Rectangle((int)_bochaPosition.X,(int)_bochaPosition.Y,40,40), Color.White);
             
                 
